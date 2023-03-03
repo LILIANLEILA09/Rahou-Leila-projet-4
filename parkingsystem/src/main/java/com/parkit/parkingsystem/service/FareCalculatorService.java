7c978c86caa1b00e3d2c0b5579e32a4883a91f84
Leila Rahou <s13b37lsxhm6-student@anonymous-codio.com>
2023-03-03 13:40:16 +0000

    corrections

--- parkingsystem/src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ parkingsystem/src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,4 +1,8 @@
 package com.parkit.parkingsystem.service;
+import java.util.concurrent.TimeUnit;
+
+import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.model.Ticket;
 
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
@@ -9,23 +13,40 @@ public class FareCalculatorService {
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
+               
+               long inHour = ticket.getInTime().getTime()/60*60*1000;
+               long outHour = ticket.getOutTime().getTime()/60*60*1000;
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+           //TODO: Some tests are failing here. Need to check if this logic is correct
+           // double  duration = outHour - inHour;
+            long timeDiffInMillies = Math.abs(ticket.getOutTime().getTime() - ticket.getInTime().getTime());
+	        double timeDiffInMinutes = TimeUnit.MINUTES.convert(timeDiffInMillies, TimeUnit.MILLISECONDS);
+	         double duration;
+             
+           // Duration fixed to 24 hours (1440 minutes) when the Time Difference is more
+	        // than 24 hours
+	    if (timeDiffInMinutes > 1440) {
+	    duration = 24;
+	   } else {
+	    duration = timeDiffInMinutes / 60;
+	  }
+        // freeRate variable controls whether the user stayed for less than 30 minutes
+	double freeRate = 1;
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
-
+	if (timeDiffInMinutes <= 30) {
+	    freeRate = 0;
+	}
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR*freeRate);
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR *freeRate);
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
         }
     }
 }
+
